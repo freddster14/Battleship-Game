c62516560c080cb4edc2774d62353d7829c5dab3
Zeronicc <zeronic4465@gmail.com>
2024-06-26 17:56:38 -0500

    Implemented Ship test

---
+++ src/ship.test.js
@@ -0,0 +1,8 @@
+import Ship from './ship';
+
+test('checks ships properties', () => {
+    const battleship = new Ship(4);
+    expect(battleship.length).toBe(4);
+    expect(battleship.timesHit).toBe(0);
+    expect(battleship.sunk).toBe(false)
+})
