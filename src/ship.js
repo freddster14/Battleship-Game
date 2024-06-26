8e402de334e47072aef2bcd26714a1186e741a9a
Zeronicc <zeronic4465@gmail.com>
2024-06-26 18:06:20 -0500

    Created Ship Class

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,13 @@
-export class Ship{
-    constructor(length){
-
+export default class Ship{
+    constructor(length, timesHit = 0, sunk = false){
+        this.length = length;
+        this.timesHit = timesHit;
+        this.sunk = sunk
+    }
+    hit(){
+        return this.timesHit += 1
+    }
+    isSunk(){
+        if(this.length === this.timesHit) return this.isSunk = true
     }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -4,5 +4,5 @@ test('checks ships properties', () => {
     const battleship = new Ship(4);
     expect(battleship.length).toBe(4);
     expect(battleship.timesHit).toBe(0);
-    expect(battleship.sunk).toBe(false)
-})
+    expect(battleship.sunk).toBe(false);
+});
