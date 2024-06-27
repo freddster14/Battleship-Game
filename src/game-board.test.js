69ca93b0a433c52ff8e5857a21c635e87d6692f8
Zeronicc <zeronic4465@gmail.com>
2024-06-27 16:49:04 -0500

    Add 2 test wrapped in describe to run attack function
    Test the isAllSunk function

--- src/game-board.test.js
+++ src/game-board.test.js
@@ -8,8 +8,8 @@ const titanic = new Ship(2)
 
 test('check if ship is in correct coordinates', () => {
     expect(game.placeShip(destroyer, [[3,3], [4,3]])).toBe(destroyer);
-    expect(game.placeShip(battleShip, [[6,6], [5,6], [4,6], [3,6], [2,6]])).toBe(battleShip);
-    expect(game.placeShip(titanic, [[3,3], [2,3]])).toBe('invalid placement')
+    expect(game.placeShip(battleShip, [[3,3], [5,6], [4,6], [3,6], [2,6]])).toBe('invalid placement');
+    expect(game.placeShip(titanic, [[1,3], [2,3]])).toBe(titanic)
 });
 
 test('see if receiveAttack hit or miss', () => {
@@ -17,4 +17,20 @@ test('see if receiveAttack hit or miss', () => {
     expect(game.receiveAttack(5,3)).toBe('miss');
     expect(game.receiveAttack(3,3)).toBe('same attack spot');
     expect(game.receiveAttack(4,3)).toBe('hit'); //sunk
+});
+
+describe('have live ships then attack to test isAllSunk', () => {
+    const ships = [destroyer, titanic];
+
+    afterEach(() => {
+        game.receiveAttack(1,3);
+        game.receiveAttack(2,3);
+    });
+    
+    test('checks ships if all sunk === false', () => {
+        expect(game.isAllSunk(ships)).toBe(false);
+    });
+    test('checks ships if all sunk === true', () => {
+        expect(game.isAllSunk(ships)).toBe(true);
+    })
 })
