c049282619232d9b5d01b0773b3c9d3a9d8c4c24
Zeronicc <zeronic4465@gmail.com>
2024-06-27 14:18:33 -0500

    Add test case for placeShip function

--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -1,16 +1,18 @@
-import Gameboard from "./gameboard";
+import GameBoard from "./gameboard";
 import Ship from "./ship"
 
-const game = new Gameboard;
-const destroyer = new Ship(2)
+const game = new GameBoard;
+const destroyer = new Ship(2);
+const battleShip = new Ship(5)
 
 test('check if ship is in correct coordinates', () => {
     expect(game.placeShip(destroyer, [3,3], [4,3])).toBe(destroyer);
+    expect(game.placeShip(battleShip, [6,6], [5,6], [4,6], [3,6], [2,6])).toBe(battleShip)
    
 });
 
 test('see if receiveAttack hit or miss', () => {
-    expect(game.receiveAttack(3,3)).toBe(destroyer.sunk); //false
-    expect(game.receiveAttack(2,6)).toBe('missed attack')
-    expect(game.receiveAttack(4,3)).toBe(destroyer.sunk); //true
+    expect(game.receiveAttack(3,3)).toBe(destroyer.sunk); //false but hit
+    expect(game.receiveAttack(2,6)).toBe('miss')
+    expect(game.receiveAttack(4,3)).toBe(destroyer.sunk); //true sunk
 })
