9020a18cc2ec6b7b18f0f4296a62b269080caf7d
Zeronicc <zeronic4465@gmail.com>
2024-06-28 17:17:27 -0500

    feat: Initialize game setup and board creation

--- src/index.js
+++ src/index.js
@@ -5,25 +5,54 @@ import styles from './style.css'
 
 const startBtn = document.querySelector('.start');
 const gameBoards = document.getElementsByClassName('board');
-startBtn.addEventListener('click', startGame())
+startBtn.addEventListener('click', () => {});
 
+const player1 = new Player;
+const player2 = new Player;
+let turn = player1;
 
-function startGame() {
-    const player1 = new Player;
-    const player2 = new Player;
-    createBoardGrid(64);
-}
+player1.gameBoard.placeShip(new Ship(3), [[4,3], [4,4],[4,5]])
+player1.gameBoard.placeShip(new Ship(2), [[6,1], [5,1]])
+player1.gameBoard.placeShip(new Ship(4), [[4,7], [3,7], [2,7], [1,7]])
+player2.gameBoard.placeShip(new Ship(2), [[0,3], [1,3]])
+player2.gameBoard.placeShip(new Ship(4), [[7,3], [6,3],[5,3], [4,3]])
+player2.gameBoard.placeShip(new Ship(3), [[5,5], [4,5],[3,5]])  
 
-function createBoardGrid(gridSize) {
-    let n = 0;
-    for(let i = 0; i < gridSize; i++){
-        let div = document.createElement('div');
-        div.className = "squares";
-        if(i === 32) n += 1;
-        gameBoards[n].appendChild(div);
-        gameBoards[n].style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-        gameBoards[n].style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+createBoardGrid(player1.gameBoard.grid, 0);
+createBoardGrid(player2.gameBoard.grid, 1)
+
+function createBoardGrid(grid, t) {
+    for(let i = 0; i < grid.length; i++){
+        for(let n in grid[i]){
+            let square = document.createElement('div');
+            square.className = "squares";
+            //Checks for Ship
+            if(grid[i][n] !== false) square.classList.add('ship');
+            //Set coordinates
+            square.id = `${i},${n}`
+            square.addEventListener('click', (e) => {
+                let board = square.parentElement.classList.contains('1')
+                let x = Number(square.id.at(0));
+                let y = Number(square.id.at(2));
+                squareEvent(x, y, board)
+                console.log(`Square Status: ${turn.gameBoard.grid[x][y]}`)
+            });
+            gameBoards[t].appendChild(square);
+            gameBoards[t].style.gridTemplateColumns = `repeat(64, 1fr)`;
+            gameBoards[t].style.gridTemplateRows = `repeat(64, 1fr)`;
+        }
+       
     }
 }
 
-export {startGame}
+function squareEvent(x, y, board){
+    if(turn === player1 && board === false) {
+        turn = player2;
+        turn.gameBoard.receiveAttack(x, y);
+    } else if(board === true && turn === player2){
+        turn = player1
+        turn.gameBoard.receiveAttack(x, y);
+    }
+}
+
+
