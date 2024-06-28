edca57c471587e3455f99a6e059ba95c32306676
Zeronicc <zeronic4465@gmail.com>
2024-06-28 13:49:43 -0500

    Set up for DOM events

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,20 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: './src/template.html',
-        title: 'Development',
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   module: {
     rules: [
       {
             test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader"],
+            use: [{ loader: MiniCssExtractPlugin.loader,}, "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -28,14 +25,17 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
     ],
   },
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+ 
 };
