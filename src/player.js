783e70a6a443fc68bc30b026779923a79d2e7ffe
Zeronicc <zeronic4465@gmail.com>
2024-06-27 19:13:03 -0500

    Add test to see if Player is working

--- src/player.js
+++ src/player.js
@@ -1,4 +1,7 @@
+import GameBoard from "./game-board"
+
 export default class Player {
     constructor() {
+        this.gameBoard = new GameBoard
     }
 }
--- src/player.test.js
+++ src/player.test.js
@@ -2,8 +2,9 @@ import Player from "./player";
 import Ship from "./ship";
 
 const player1 = new Player;
+const comp = new Player;
 const boat = new Ship(2)
 
 test('check file', () => {
-    expect(player1.gameBoard.isAllSunk(...boat)).toBe(false)
+    expect(player1.gameBoard.isAllSunk([boat])).toBe(false)
 })
